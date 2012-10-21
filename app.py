1735c595d48d3f71388bbcbe1dfcf354091e9c29
Liyan David Chang <liyanchang@mit.edu>
2012-10-21 11:41:23 -0700

    app with endpoints

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/<handle>/<version>')
+def get_version(handle, version):
+    return "Getting handle: %s version: %s" % (handle, version)
+
+@app.route('/<handle>', methods=['GET', 'POST'])
+def root_handle(handle):
+    if request.method == "POST":
+        return "SAVING"
+    else:
+        return "Getting handle: %s recent version" % (handle)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
