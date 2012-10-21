abe20e9cc6d475e3317e727f0d4672803fd40a2e
Liyan David Chang <liyanchang@mit.edu>
2012-10-21 11:53:35 -0700

    redid endpoints. allow for new

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/<handle>/<version>')
+@app.route('/file/<handle>/<version>')
 def get_version(handle, version):
     return "Getting handle: %s version: %s" % (handle, version)
 
-@app.route('/<handle>', methods=['GET', 'POST'])
+@app.route('/file/<handle>', methods=['GET', 'POST'])
 def root_handle(handle):
     if request.method == "POST":
         return "SAVING"
     else:
         return "Getting handle: %s recent version" % (handle)
+
+@app.route('/new/'):
+    return "new file handle"
 
 if __name__ == '__main__':
     app.run(debug=True)
