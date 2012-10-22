db86bbfcbca0d444be2617b40ea7a1baac85f0b6
Liyan David Chang <liyanchang@mit.edu>
2012-10-21 17:06:01 -0700

    version download; filename updates

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import uuid
 import os.path
 import time
 
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
+
 app = Flask(__name__)
 
 repoName = None
@@ -14,7 +19,7 @@ def hello_world():
 
 @app.route('/<handle>/<version>')
 def get_version(handle, version):
-    return "Getting handle: %s version: %s" % (handle, version)
+    return getFile(handle, version)
 
 @app.route('/<handle>', methods=['GET', 'POST'])
 def root_handle(handle):
@@ -57,12 +62,15 @@ def root_handle(handle):
                 f = open(repoName+"/files/"+id, 'w')
                 f.write(newFile.stream.read())
                 f.close()
+                fname = open(repoName+"/files/"+id+'-filename', 'w')
+                fname.write(newFile.filename)
+                fname.close()
 
                 repo = Repo(repoName)
                 index = repo.index
 
                 index.add([repoName+"/files/"+id])
-
+                index.add([repoName+"/files/"+id+'-filename'])
                 commit = index.commit("changed file with uuid "+id+ " at "+str(time.time()))
 
                 saved = True
@@ -74,16 +82,37 @@ def root_handle(handle):
         if saved:
             return "saved new file to "+str(id)
         else:
+
             return "could not save file"
 
     else:
-        f = open(repoName + '/files/' + handle)
-        filename = getFilename(handle)
-        return send_file(f, as_attachment=True, attachment_filename=filename)
+        return getFile(handle)
 
 def getFilename(handle):
     fname = open(repoName + '/files/' + handle + "-filename")
     return fname.readline()
+
+def getFile(handle, version=None):
+    if not version:
+        f = open(repoName + '/files/' + handle)
+    else:
+        sha = versionSha(handle, version=version)
+        g = Git(repoName)
+        output = g.show('%s:files/%s' % (sha, handle))
+        f = StringIO()
+        f.write(output)
+        f.seek(0)
+
+    filename = getFilename(handle)
+    return send_file(f, as_attachment=True, attachment_filename=filename)
+
+def versionSha(handle, version=None):
+    g = Git(repoName)
+    shas = g.log('--pretty=%H','--follow','--','files/'+handle).split('\n')
+    shas.reverse()
+    if not version:
+        version = 0
+    return shas[int(version)]
 
 ## Get the repoName from our config
 def parseConfig():
